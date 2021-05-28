3002192175decd3ecfcdfba151f1550ee26bae1c
Ziya-Meiramova <ziya.meiramova@gmail.com>
2021-05-28 21:34:25 +0600

    first lesson

---
+++ 1.py
@@ -0,0 +1,125 @@
+# a = "Hi"
+
+# a = 6
+# print(a)
+# a = True
+# print(a)
+# print(type(a))
+
+# name = "Ziya"
+# surname = "Meiramova"
+# print(surname, name)
+
+
+# a = 5
+# n = "Decode"
+# print(a + n)
+
+# print(True)  #True
+# a = True
+# print(a) #True
+# print(int(a)) #1
+# print(bool(1)) #True
+# print(int(False)) # 0
+
+# a = 5
+# b = 5.5
+# c = "jdfbsflj"
+# print(type(a), type(b), type(c))
+
+# a = 2
+# b = 3
+# print(a + b, a - b, a * b, a / b, a // b, a % b, a**b)
+
+# c = 7
+# c += 4
+# c = c + 4
+# print(c)
+
+# a = 19
+# a-=3
+# print(a)
+# a*=2
+# print(a)
+# a/=2
+# print(a)
+
+# name = input()
+# surname = input("Введите свою фамилию: ")
+# age = int(input("Введите свой возраст: "))
+
+# print(surname, name, age + 1)
+
+# a = int(input())
+# b = int(input())
+
+# c = (a**2 + b**2)**0.5
+
+# print(int(c))
+
+# name = input()
+# print("Hi,", name)
+
+# a = int(input())
+# print("Пред цифра: ", a-1)
+# print("След цифра:", a+1)
+
+# s = 109
+# v = int(input())
+# t = int(input())
+
+# print((v*t) % s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
