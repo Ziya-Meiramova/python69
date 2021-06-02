13d1afe0b8510506acc4f2673f90e0142f156e74
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-02 18:08:44 +0600

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,107 @@
+#1
+# age = int(input("Enter your age: "))
+#
+# if age >= 21:
+#     print("Come in")
+# elif age <= 20 and age>=18:
+#     print("With brother")
+# else:
+#     print("Stop")
+
+#2
+#
+# a = int(input())
+# if a == 5:
+#     print("OK")
+# elif a != 10:
+#     print("OOK")
+# else:
+#     print("NOT OK")
+
+#3
+# a = int(input())
+# b = int(input())
+#
+# if a>b:
+#     print(a)
+#     print("HI")
+#     print("JKl")
+# elif b > a:
+#     print(b)
+# else:
+#     print("Equal")
+
+# #4
+# milk = int(input()) #300
+# bread = int(input()) #150
+#
+# if milk + bread <= 500:
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+# # 5
+# year = int(input())
+#
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print("Leap_year")
+# else:
+#     print("NOT")
+#
+
+#6
+# x = int(input()) #0
+# if x > 0:
+#     print(1)
+# elif x < 0:
+#     print(-1)
+# else:
+#     print(0)
+
+# #7
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a+b > c and a+c>b and b+c > a:
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+#8
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if a == b:
+#     print("OK")
+# elif b == c:
+#     print("OK")
+# elif a == c:
+#     print("OK"
+#
+# if a==b or b == c or a == c: # написали с оператором "or"
+#     print("OK")
+
+# #9
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+#
+# if x1 == x2 or y1 == y2: #or = или
+#                          # and = и
+#     print("OK")
+# else:
+#     print("NO")
+
+#10
+# a = 7
+# if a != 9:
+#     print("NO")
+# else:
+#     print("YES")
+
+
+
+
+
