60040b69c71bc0777fd85898c17eb8afd312e652
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-04 21:13:43 +0600

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,105 @@
+# cnt = 0 бесконечный цикл
+# a = 0
+# while a == 0:
+#     cnt+=1
+#     print(cnt)
+
+# cnt = 10
+# while cnt >= 0:
+#     cnt = cnt - 2
+#     print("Hi", cnt)
+
+# a = 5
+# while a >= 0:
+#     print(a)
+#     a+= 1
+#
+# print("Stop")
+
+# while True:
+#     line = input()
+#     if line == "Stop":
+#         break
+#     print(line) # повторяет строку которую ввели
+# print("End")
+
+
+# a = int(input()) #6
+#
+# while a > 0:
+#     a -= 1
+#     print(a)
+#     if a == 5:
+#         continue
+#     elif a == 2:
+#         break
+#     print(a)
+
+# cnt = 0
+# while cnt <= 10:
+#     cnt = cnt + 1
+#     if cnt % 2 != 0:
+#         continue
+#     print(cnt)
+
+# cnt = 1
+# while cnt > 0:
+#     cnt+=2
+
+# while True:
+#     print("J")
+#     a = input()
+#     if a == "S":
+#         break
+
+#
+# a = 90
+# print(a)
+# print("HI")
+# if a == 90:
+#     print("YES")
+#
+# while a > 0:
+#     pass  # сделаешь сам
+# v = 9
+# print (v)
+
+# cnt = 2
+# n = int(input()) #9
+# while cnt<=n:
+#     if n%cnt == 0:
+#         print(cnt)
+#         break
+#     cnt+=1
+
+
+# n = int(input())
+# cnt = 0
+#
+# while cnt <= n:
+#     cnt+=1
+#     if 2**cnt == n:
+#         print("YES")
+#         break
+# else:
+#     print("NO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
