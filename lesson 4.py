a22e2104db64d04c818d17c8c490028959f34caf
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-07 22:27:32 +0600

    Add files via upload

---
+++ lesson 4.py
@@ -0,0 +1,92 @@
+#while x < y:
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+# for cnt in range(2, 10):
+#
+#     print(cnt)
+#     if cnt % 2 != 0:
+#         continue
+#     print(cnt)
+# print(cnt)
+
+# for i in range(2, 10):
+#     if i % 2 != 0:
+#         continue
+#     if i == 6:
+#         break
+#     print(i)
+
+# A informatics
+# a = int(input())
+# b = int(input())
+#
+# for i in range(a, b+1):
+#     # if i % 2 == 0: #1 вариант решения
+#     #     print(i, end = " ")
+#     if i % 2 != 0:  #2 вариант решения
+#         continue
+#     print(i)
+
+# for i in range(2, 6):
+#     print(i)
+
+#B
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+# for i in range(a, b+1):
+#     if i % d != c:
+#         continue
+#     print(i)
+
+# #C
+# a = int(input())
+# b = int(input())
+#
+# for i in range(b+1):
+#     if i**2 >= a and i**2 <= b:
+#         print(i**2)
+
+
+#G
+# x = int(input())
+#
+# for i in range(2, x+1):
+#     if x % i == 0:
+#         print(i)
+#         break
+
+# n = int(input())
+# cnt = 0
+# for i in range(1,n+1):
+#     if i % 2 == 0:
+#         cnt+=1
+# print(cnt)
+
+# n = int(input())
+# sum = 0
+# for i in range(n):
+#     a = int(input())
+#     if a % 2 != 0:
+#         sum +=a
+# print(sum)
+
+# n = int(input())
+# mx = -1
+# for i in range(n): #1 6 99 4 0
+#     a = int(input())
+#     #print(max(a))
+#     if a > mx:
+#         mx = a
+# print(mx)
+
+
+
+
+
+
+
