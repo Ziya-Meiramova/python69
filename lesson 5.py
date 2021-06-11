989542ac7f5ed3f64a80aa4c7598a696efc82dc4
Ziya-Meiramova <84172592+Ziya-Meiramova@users.noreply.github.com>
2021-06-11 21:02:50 +0600

    Add files via upload

---
+++ lesson 5.py
@@ -0,0 +1,65 @@
+a = 5
+b = 0
+
+a = [1, 2.4, 3, 67, 3, 3]
+# print(a)
+# print(len(a))
+# for i in range(len(a)):
+#     print(a[i] + 2, end=" ")
+# a.append("Hello") #добавить эл с конца
+# print(a)
+# a.insert(2, "asd") #добавить эл по индексу
+# a.remove(3) #удаление по значению
+# a.pop() #удаление последний эл
+# print(a.count(3))
+# b = [-9, 6, 0, -7]
+# b.sort() #сортировка
+# print(b)
+# b.reverse() #переворачивает массив
+# print(b)
+# #b.clear() #удаляет весь массив
+# print(b)
+# c = a #дубликат массива
+# c = a.copy() #дубликат массива
+# c = a + b
+# print(type(c))
+#
+# s = (1, 2, 3)
+# print(type(s))
+# d = list(s)
+# print(type(d))
+
+a = "hello World"
+print(a[1])
+print(a[::3])
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.find('l'))
+print(a[a.find('l')].upper())
+print(a.isascii())
+print(a.isdigit())
+
+# d = """Hello!
+# I am Ziya!
+# Nice to meet you"""
+# print(d)
+
+# #format
+# b = 6
+# a = 4
+# o = 3
+# txt = "I have {} banana, {} apple, {} orange"
+# print(txt.format(b, a, o))
+
+txt = " Hi W "
+# x = txt.strip()
+# print(x)
+txt.upper()
+
+
+
+
+
+
+
